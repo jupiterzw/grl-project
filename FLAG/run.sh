88bc5e8cc794b92af7606264966e9534c3c13319
Zhiyu Wang (Peter) <105394372+jupiterzw@users.noreply.github.com>
2025-01-08 05:15:55 +0000

    FLAG

---
+++ FLAG/run.sh
@@ -0,0 +1,7 @@
+
+# ogb-arxiv
+
+python main.py --method flag --gnn sage --lr 0.01 --flag_step_size 0.005 --flag_ascent_steps 3 --dataset ogb-arxiv --device 0
+python main.py --method flag --gnn gpr --lr 0.01 --flag_step_size 0.005 --flag_ascent_steps 3 --dataset ogb-arxiv --device 0
+
+
